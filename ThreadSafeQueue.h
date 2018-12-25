aed8267bd4907f20703c1de437a41289626280d2
hnandag2 <46095910+hnandag2@users.noreply.github.com>
2018-12-24 20:26:36 -0800

    Thread safe queue

---
+++ ThreadSafeQueue.h
@@ -0,0 +1,59 @@
+//
+//  ThreadSafeQueue.h
+//
+//  single lock (ie: not fine-grained) thread-safe queue implemented using a mutex
+//  and a condition variable. All accesses will be serialized, however waitAndPop()
+//  will always wait for the queue to not be empty.
+//
+//  Created by Nandagopal, Harihar on 12/24/18.
+//
+
+#pragma once
+
+#include <mutex>
+#include <queue>
+#include <condition_variable>
+
+
+template <typename T>
+class ThreadSafeQueue
+{
+public:
+    ThreadSafeQueue() {}
+    
+    bool tryPop(T& value)
+    {
+        std::lock_guard<std::mutex> lg(mLock);
+        if (mDataQueue.empty()) return false;
+        value = mDataQueue.front();
+        mDataQueue.pop();
+        return true;
+    }
+    
+    void waitAndPop(T& value)
+    {
+        std::unique_lock<std::mutex> lg(mLock);
+        mDataCond.wait(lg, [this]() { return !mDataQueue.empty();});
+        value = mDataQueue.front();
+        mDataQueue.pop();
+    }
+    
+    void push(T val)
+    {
+        std::lock_guard<std::mutex> lg(mLock);
+        mDataQueue.push(val);
+        mDataCond.notify_one();
+    }
+    
+    bool empty()
+    {
+        std::lock_guard<std::mutex> lg(mLock);
+        return mDataQueue.empty();
+    }
+    
+    
+private:
+    std::queue<T> mDataQueue;
+    mutable std::mutex mLock;
+    std::condition_variable mDataCond;
+};
